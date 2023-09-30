088464e0a361e5a17dd2fd5ac85d1034b4f4ed65
Monika <raciam@oregonstate.edu>
2023-09-30 15:39:17 -0500

    Set up folder structure

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Contact from './pages/Contact'
+
+function App() {
+  return (
+    <div className="App">
+      <Contact />
+
+    </div>
+  );
+}
+
+export default App;
