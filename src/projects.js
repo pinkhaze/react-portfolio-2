24be82ad4e42509ac2a14c31c013839b0013d9c0
Monika <raciam@oregonstate.edu>
2023-10-05 07:05:10 -0500

    Update projects

--- src/projects.js
+++ src/projects.js
@@ -1,12 +1,15 @@
 import WeatherDashboardImg from './assets/images/weather-dashboard.png';
 import MakeMyDrinkImg from './assets/images/make-my-drink.png';
-
+import NoteTakerImg from './assets/images/note-taker.png';
+import PursuitOfMusicImg from './assets/images/pursuit-of-music.png';
+import WorkdaySchedulerImg from './assets/images/workday-scheduler.png';
+import PWATextEditorImg from './assets/images/pwa-text-editor.png';
 
 const projects = [
   {
     title: 'Make My Drink',
     description: '',
-    technologies: 'JS, Bulma',
+    technologies: 'JS, Bulma and HTML',
     repository: 'https://github.com/brownKasey/Make-My-Drink',
     deployed: 'https://brownkasey.github.io/Make-My-Drink/',
     preview: MakeMyDrinkImg,
@@ -15,46 +18,46 @@ const projects = [
   {
     title: 'Weather Dashboard',
     description: '',
-    technologies: 'JS, Bulma',
+    technologies: 'JS, HTML and CSS',
     repository: 'https://github.com/pinkhaze/weather-dashboard',
     deployed: 'https://pinkhaze.github.io/weather-dashboard',
     preview: WeatherDashboardImg,
     alt: 'weather icons',
   },
   {
-    title: 'Make My Drink',
+    title: 'Note Taker',
     description: '',
     technologies: 'JS, Bulma',
-    repository: 'https://github.com/pinkhaze/weather-dashboard',
-    deployed: 'https://pinkhaze.github.io/weather-dashboard',
-    preview: WeatherDashboardImg,
+    repository: 'https://github.com/pinkhaze/note-taker',
+    deployed: 'https://pinkhaze.github.io/note-taker',
+    preview: NoteTakerImg,
     alt: 'weather icons',
   },
   {
-    title: 'Make My Drink',
+    title: 'Pursuit of Music',
     description: '',
-    technologies: 'JS, Bulma',
+    technologies: 'JS, Bulma, Sequelize, mySQL and Bcrypt',
     repository: 'https://github.com/pinkhaze/weather-dashboard',
     deployed: 'https://pinkhaze.github.io/weather-dashboard',
-    preview: WeatherDashboardImg,
+    preview: PursuitOfMusicImg,
     alt: 'weather icons',
   },
   {
-    title: 'Make My Drink',
+    title: 'Workday Scheduler',
     description: '',
-    technologies: 'JS, Bulma',
-    repository: 'https://github.com/pinkhaze/weather-dashboard',
-    deployed: 'https://pinkhaze.github.io/weather-dashboard',
-    preview: WeatherDashboardImg,
+    technologies: 'JS, CSS and HTML',
+    repository: 'https://github.com/pinkhaze/workday-scheduler',
+    deployed: 'https://pinkhaze.github.io/workdday-scheduler',
+    preview: WorkdaySchedulerImg,
     alt: 'weather icons',
   },
   {
-    title: 'Make My Drink',
+    title: 'PWA Text Editor',
     description: '',
-    technologies: 'JS, Bulma',
-    repository: 'https://github.com/pinkhaze/weather-dashboard',
-    deployed: 'https://pinkhaze.github.io/weather-dashboard',
-    preview: WeatherDashboardImg,
+    technologies: 'JS, Node and Webpack',
+    repository: 'https://github.com/pinkhaze/pwa-text-editor',
+    deployed: 'https://pinkhaze.github.io/pwa-text-editor',
+    preview: PWATextEditorImg,
     alt: 'weather icons',
   },
 ];
